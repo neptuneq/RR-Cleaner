1bd5eaddc6358b8d67214f8006733b9066718426
neptuneq <110648150+neptuneq@users.noreply.github.com>
2023-01-02 00:09:49 -0800

    hotfix

--- nep's RR cleaner v3 (fast).bat
+++ nep's RR cleaner v3 (fast).bat
@@ -1,7 +1,7 @@
 @echo off
 color 9
 echo ======================================================================================
-echo Rec Room Data Cleaner v3 (fast edition) Made by neptune#1995
+echo Rec Room Data Cleaner v3.1 (fast edition) Made by neptune#1995
 echo ======================================================================================
 echo This script attempts to clean all of Rec Room's data from your computer.
 echo Run this without administrator cause file paths will mess up.
@@ -10,13 +10,11 @@ echo ===========================================================================
 echo Deleting Rec Room Local Low
 rmdir /s /q "C:\Users\%USERNAME%\AppData\LocalLow\Against Gravity"
 echo Deleting Rec Room's Temp Files
-rmdir /s /q "C:\Users\%USERNAME%\AppData\Local\Temp\RecRoom"
+rmdir /s /q "%LocalAppData%\Temp\RecRoom
+rmdir /s /q "%LocalAppData%\Temp\Against Gravity"
 echo Deleting Rec Room's Microsoft Files
-rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Recent\RecRoom.lnk"
-rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Rec Room.url"
-echo Deleting Rec Room's Windows Files
-rmdir /s /q "C:\Windows\Prefetch\RECROOM.EXE-BEC42EED.pf"
-rmdir /s /q "C:\Windows\Prefetch\RECROOM_RELEASE.EXE-35556F3D.pf"
+del /s /q %AppData%\Microsoft\Windows\Recent\RecRoom.lnk
+del /s /q "%AppData%\Microsoft\Windows\Start Menu\Programs\Steam\Rec Room.url"
 echo Completed File Deletion
 timeout /t 1 /nobreak > nul
 echo ======================================================================================
